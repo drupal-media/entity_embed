11e0095abb69a1e21753a495edcc78a0657409b1
Chandan Singh <chandan.devel@gmail.com>
2014-06-06 22:39:39 +0530

    Use t function for value of back button

--- src/Form/EntityEmbedCKEditorEmbedForm.php
+++ src/Form/EntityEmbedCKEditorEmbedForm.php
@@ -82,7 +82,7 @@ class EntityEmbedCKEditorEmbedForm extends FormBase {
     $form['actions'] = array('#type' => 'actions');
     $form['actions']['go_back'] = array(
       '#type' => 'submit',
-      '#value' => 'Back',
+      '#value' => '$this->t(Back'),
       // No regular submit-handler. This form only works via JavaScript.
       '#submit' => array(),
       '#ajax' => array(

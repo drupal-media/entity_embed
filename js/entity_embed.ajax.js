dc569748a9d0287fe089fbbd11af4cbee31eb29d
Chandan Singh <chandan.devel@gmail.com>
2014-06-11 01:33:46 +0530

    Add all attributes using a for loop instead of hard-coding those

--- js/entity_embed.ajax.js
+++ js/entity_embed.ajax.js
@@ -18,22 +18,10 @@
       // Prepare the data attributes from supplied values.
       var entityDiv = document.createElement('div');
 
-      // Set entity type.
-      entityDiv.setAttribute('data-entity-type', attributes.data_entity_type);
-
-      // Set entity UUID.
-      entityDiv.setAttribute('data-entity-uuid', attributes.data_entity_uuid);
-
-      // Set view mode.
-      entityDiv.setAttribute('data-view-mode', attributes.data_view_mode);
-
-      // Set show caption attribute, only if its set in the form.
-      if(attributes.show_caption == 1) {
-        entityDiv.setAttribute('data-show-caption', 'data-show-caption');
+      // Set all the supplied data attributes.
+      for (var key in attributes) {
+        entityDiv.setAttribute(key, attributes[key]);
       }
-
-      // Set a placeholder.
-      entityDiv.innerHTML = attributes.data_entity_type + ": " + attributes.data_entity_uuid;
 
       // Generate HTML of the DOM Object.
       var entityHTML = entityDiv.outerHTML;

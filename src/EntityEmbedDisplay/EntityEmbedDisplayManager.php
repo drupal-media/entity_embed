cc9d6f915113b933013a3dfce5820d20c155d331
Dave Reid <dave@davereid.net>
2014-06-12 15:12:46 -0500

    Fixing default plugin manager no longer needs LanguageManager. Chasing fixed core issue https://drupal.org/node/2281905.

--- src/EntityEmbedDisplay/EntityEmbedDisplayManager.php
+++ src/EntityEmbedDisplay/EntityEmbedDisplayManager.php
@@ -35,10 +35,10 @@ class EntityEmbedDisplayManager extends DefaultPluginManager {
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler to invoke the alter hook with.
    */
-  public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, LanguageManager $language_manager, ModuleHandlerInterface $module_handler) {
+  public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
     parent::__construct('EntityEmbedDisplay', $namespaces, $module_handler, 'Drupal\entity_embed\Annotation\EntityEmbedDisplay');
     $this->alterInfo('entity_embed_display_plugins');
-    $this->setCacheBackend($cache_backend, $language_manager, 'entity_embed_display_plugins');
+    $this->setCacheBackend($cache_backend, 'entity_embed_display_plugins');
   }
 
   /**

b8a14f4bcd42e3bff46f8e8474d927b603f729b0
Chandan Singh <chandan.devel@gmail.com>
2014-06-17 01:44:49 +0530

    Add php docblocks

--- src/EntityEmbedController.php
+++ src/EntityEmbedController.php
@@ -10,8 +10,16 @@ namespace Drupal\entity_embed;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ * Returns responses for EntityEmbed module routes.
+ */
 class EntityEmbedController extends ControllerBase {
 
+  /**
+   * Returns an Ajax response to generate preview of an entity.
+   *
+   * Expects the data attributes to be provided as GET parameters.
+   */
   public function preview() {
     return new JsonResponse(array(
       'content' => "Entity preview here!",

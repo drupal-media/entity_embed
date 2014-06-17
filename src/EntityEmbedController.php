f65b0957c013df75aef8b2a1b9c2178e3d0c4bc7
Chandan Singh <chandan.devel@gmail.com>
2014-06-17 22:52:00 +0530

    Return rendered as preview

--- src/EntityEmbedController.php
+++ src/EntityEmbedController.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\entity_embed;
 
+use Drupal\Component\Serialization\Json;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -14,6 +15,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  * Returns responses for EntityEmbed module routes.
  */
 class EntityEmbedController extends ControllerBase {
+  use EntityHelperTrait;
 
   /**
    * Returns an Ajax response to generate preview of an entity.
@@ -21,8 +23,24 @@ class EntityEmbedController extends ControllerBase {
    * Expects the data attributes to be provided as GET parameters.
    */
   public function preview() {
+    $context = \Drupal::request()->query->all();
+    $entity_embed_settings = Json::decode($context['data-entity-embed-settings']);
+    $entity_output = 'No preview available.';
+    try {
+      if ($entity = $this->loadEntity($context['data-entity-type'], $context['data-entity-id'])) {
+        $entity_output = $this->renderEntityEmbedDisplayPlugin(
+          $entity,
+          $context['data-entity-embed-display'],
+          $entity_embed_settings,
+          $context
+        );
+      }
+    }
+    catch (\Exception $e) {
+      watchdog_exception('entity_embed', $e);
+    }
     return new JsonResponse(array(
-      'content' => "Entity preview here!",
+      'content' => $entity_output,
     ));
   }
 

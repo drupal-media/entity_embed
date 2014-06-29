e5206f8edd1882bfb53d163c9fd00fa59c6ddeb9
Chandan Singh <chandan.devel@gmail.com>
2014-06-30 00:19:00 +0530

    Use proper GET attributes while passing value

--- src/Tests/EntityEmbedPreviewTest.php
+++ src/Tests/EntityEmbedPreviewTest.php
@@ -75,7 +75,7 @@ class EntityEmbedPreviewTest extends WebTestBase {
   public function testPreviewController() {
     $content = '<div data-entity-type="node" data-entity-id="' . $this->node->id() . '" data-view-mode="teaser">This placeholder should not be rendered.</div>';
 
-    $html = $this->drupalGet($this->preview_url, array('value' => 'Test'));
+    $html = $this->drupalGet($this->preview_url, array('query' => array('value' => $content)));
 
     $this->assertResponse(200, 'The preview route exists.');
     $this->assertText($this->embedContent, 'Embedded node exists in page');
@@ -86,7 +86,7 @@ class EntityEmbedPreviewTest extends WebTestBase {
    * Tests preview route with an invalid request.
    */
   public function testPreviewControllerInvalidRequest() {
-    $html = $this->drupalGet($this->preview_url, array('value' => 'Testing preview route without valid values'));
+    $html = $this->drupalGet($this->preview_url, array('query' => array('value' => 'Testing preview route without valid values')));
 
     $this->assertResponse(200, 'The preview route exists.');
     $this->assertText('Testing preview route without valid values', 'Placeholder appears in the output when embed is unsuccessful.');

075b1d4c26a4bc5df1f9417efccc518dbb636860
Steve Persch <persch@palantir.net>
2014-07-07 21:01:05 -0500

    Adding EntityHelperTrait to EntityEmbedTwigExtension.php

--- src/Twig/EntityEmbedTwigExtension.php
+++ src/Twig/EntityEmbedTwigExtension.php
@@ -8,27 +8,30 @@
 namespace Drupal\entity_embed\Twig;
 
 use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\entity_embed\EntityHelperTrait;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\entity_embed\EntityEmbedDisplay\EntityEmbedDisplayManager;
 
 /**
  * Provide entity embedding function within Twig templates.
  */
 class EntityEmbedTwigExtension extends \Twig_Extension {
+  use EntityHelperTrait;
 
   /**
-   * The entity manager.
-   *
-   * @var \Drupal\Core\Entity\EntityManagerInterface
-   */
-  protected $entityManager;
-
-  /**
-   * Constructs a new EntityFormBuilder.
+   * Constructs a new EntityEmbedTwigExtension.
    *
    * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
-   *   The entity manager.
+   *   The entity manager service.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   * @param \Drupal\entity_embed\EntityEmbedDisplay\EntityEmbedDisplayManager $plugin_manager
+   *   The plugin manager.
    */
-  public function __construct(EntityManagerInterface $entity_manager) {
-    $this->entityManager = $entity_manager;
+  public function __construct(EntityManagerInterface $entity_manager, ModuleHandlerInterface $module_handler, EntityEmbedDisplayManager $plugin_manager) {
+    $this->setEntityManager($entity_manager);
+    $this->setModuleHandler($module_handler);
+    $this->setDisplayPluginManager($plugin_manager);
   }
 
   /**
@@ -43,18 +46,18 @@ class EntityEmbedTwigExtension extends \Twig_Extension {
    */
   public function getFunctions() {
     return array(
-      new \Twig_SimpleFunction('entity_embed', array($this, 'getRenderArrayByMachineName')),
+      new \Twig_SimpleFunction('entity_embed', array($this, 'getRenderArray')),
     );
   }
 
   /**
-   * Return the render array for a block instance.
+   * Return the render array for an entity.
    *
    * @param string $entity_type
    *   The machine name of an entity_type like 'block'.
    *
    * The function is used in conjunction with Twig to be able to call in a Twig
-   * file something like {{ entity_embed('machine_name_of_block') }}
+   * file something like {{ entity_embed('block', 'machine_name_of_block') }}
    *
    * @param string $entity_id
    *   The machine name of a block instance like 'poweredbydrupal_2' or
@@ -66,19 +69,23 @@ class EntityEmbedTwigExtension extends \Twig_Extension {
    *   The machine name of an view mode like 'default' or 'teaser'.
    *
    * @return array
-   *   A render array from entity_view()
+   *   A render array from entity_view().
    */
-  public function getRenderArrayByMachineName($entity_type, $entity_id, $view_mode = 'default') {
+  public function getRenderArray($entity_type, $entity_id, $view_mode = 'default') {
 
     // These lines are essentially entity_load() and entity_view() without
     // the reset option(). The injected entityManager property is used
     // instead of procedural functions for eventual unit-testability.
     $controller = $this->entityManager->getStorage($entity_type);
-    $entity = $controller->load($entity_id);
-    // entity_view equivilent.
-    $render_controller = $this->entityManager->getViewBuilder($entity->getEntityTypeId());
+    $entity = $this->loadEntity($entity_type, $entity_id);
+
     // @todo, handling for langcode variable.
-    $build = $render_controller->view($entity, $view_mode);
+    $plugin_configuration = array(
+      'view_mode'=> $view_mode,
+    );
+    // @todo, handling for changing plugin_ids.
+    $plugin_id = 'default';
+    $build = $this->renderEntityEmbedDisplayPlugin($entity, $plugin_id, $plugin_configuration);
 
     return $build;
   }

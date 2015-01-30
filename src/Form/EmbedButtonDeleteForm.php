c4d453c76cbe0572ca3a1ec50ec17a6a74685f58
Chandan Singh <chandan.devel@gmail.com>
2015-01-30 18:00:50 +0530

    Use EntityDeleteForm to handle entity deletion

--- src/Form/EmbedButtonDeleteForm.php
+++ src/Form/EmbedButtonDeleteForm.php
@@ -7,43 +7,19 @@
 
 namespace Drupal\entity_embed\Form;
 
-use Drupal\Core\Entity\EntityConfirmFormBase;
+use Drupal\Core\Entity\EntityDeleteForm;
 use Drupal\Core\Url;
-use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Builds the form to delete an Embed Button.
  */
-class EmbedButtonDeleteForm extends EntityConfirmFormBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getQuestion() {
-    return $this->t('Are you sure you want to delete %name?', array('%name' => $this->entity->label()));
-  }
+class EmbedButtonDeleteForm extends EntityDeleteForm {
 
   /**
    * {@inheritdoc}
    */
   public function getCancelUrl() {
     return new Url('embed_button.list');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getConfirmText() {
-    return $this->t('Delete');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->entity->delete();
-    drupal_set_message($this->t('Button %label has been deleted.', array('%label' => $this->entity->label())));
-    $form_state->setRedirectUrl($this->getCancelUrl());
   }
 
 }

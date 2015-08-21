407f48ea0d90682b79c63309793a6642e51b397a
Dave Reid <dave@davereid.net>
2015-08-21 00:28:29 -0500

    Reuse EmbedCKEditorPluginBase.

--- src/Plugin/CKEditorPlugin/DrupalEntity.php
+++ src/Plugin/CKEditorPlugin/DrupalEntity.php
@@ -7,13 +7,9 @@
 
 namespace Drupal\entity_embed\Plugin\CKEditorPlugin;
 
-use Drupal\Component\Utility\SafeMarkup;
-use Drupal\Core\Entity\Query\QueryInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\ckeditor\CKEditorPluginBase;
 use Drupal\editor\Entity\Editor;
-use Drupal\embed\Entity\EmbedButton;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\embed\EmbedButtonInterface;
+use Drupal\embed\EmbedCKEditorPluginBase;
 
 /**
  * Defines the "drupalentity" plugin.
@@ -21,68 +17,18 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @CKEditorPlugin(
  *   id = "drupalentity",
  *   label = @Translation("Entity"),
- *   module = "entity_embed"
+ *   embed_type_id = "entity"
  * )
  */
-class DrupalEntity extends CKEditorPluginBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The embed button query.
-   *
-   * @var \Drupal\Core\Entity\Query\QueryInterface
-   */
-  protected $embedButtonQuery;
-
-  /**
-   * Constructs a Drupal\entity_embed\Plugin\CKEditorPlugin\DrupalEntity object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\Query\QueryInterface $embed_button_query
-   *   The entity query object for embed button.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, QueryInterface $embed_button_query) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->embedButtonQuery = $embed_button_query;
-    $this->embedButtonQuery->condition('type_id', 'entity');
-  }
+class DrupalEntity extends EmbedCKEditorPluginBase {
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity.query')->get('embed_button')
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getButtons() {
-    $buttons = array();
-
-    if ($ids = $this->embedButtonQuery->execute()) {
-      $embed_buttons = EmbedButton::loadMultiple($ids);
-      foreach ($embed_buttons as $button) {
-        $buttons[$button->id()] = array(
-          'id' => $button->id(),
-          'name' => SafeMarkup::checkPlain($button->label()),
-          'label' => SafeMarkup::checkPlain($button->label()),
-          'entity_type' => $button->getTypeSetting('entity_type'),
-          'image' => $button->getIconUrl(),
-        );
-      }
-    }
-
-    return $buttons;
+  protected function getButton(EmbedButtonInterface $embed_button) {
+    $button = parent::getButton($embed_button);
+    $button['entity_type'] = $embed_button->getTypeSetting('entity_type');
+    return $button;
   }
 
   /**
@@ -95,22 +41,11 @@ class DrupalEntity extends CKEditorPluginBase implements ContainerFactoryPluginI
   /**
    * {@inheritdoc}
    */
-  public function getLibraries(Editor $editor) {
-    return array(
-      'embed/embed',
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function getConfig(Editor $editor) {
-    $buttons = $this->getButtons();
-
     return array(
       'DrupalEntity_dialogTitleAdd' => t('Insert entity'),
       'DrupalEntity_dialogTitleEdit' => t('Edit entity'),
-      'DrupalEntity_buttons' => $buttons,
+      'DrupalEntity_buttons' => $this->getButtons(),
     );
   }
 

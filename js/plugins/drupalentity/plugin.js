99404669a34ee92e466e3444f1f053220a0a0512
Kevin Moll <kevin.j.moll@gmail.com>
2016-01-26 16:31:58 -0500

    Issue #2638788 by slashrsm, Berdir, Reinmar, Wim Leers, kmoll: Required attributes wildcard in CKEditor plugin not compatible with filter system.

--- js/plugins/drupalentity/plugin.js
+++ js/plugins/drupalentity/plugin.js
@@ -27,8 +27,8 @@
 
       // Generic command for adding/editing entities of all types.
       editor.addCommand('editdrupalentity', {
-        allowedContent: 'drupal-entity[*]',
-        requiredContent: 'drupal-entity[*]',
+        allowedContent: 'drupal-entity[data-entity-type,data-entity-id,data-entity-uuid,data-entity-embed-display,data-entity-embed-settings]',
+        requiredContent: 'drupal-entity[data-entity-type,data-entity-id,data-entity-uuid,data-entity-embed-display,data-entity-embed-settings]',
         modes: { wysiwyg : 1 },
         canUndo: true,
         exec: function (editor, data) {
@@ -83,8 +83,8 @@
       // Register the entity embed widget.
       editor.widgets.add('drupalentity', {
         // Minimum HTML which is required by this widget to work.
-        allowedContent: 'drupal-entity[*]',
-        requiredContent: 'drupal-entity[*]',
+        allowedContent: 'drupal-entity[data-entity-type,data-entity-id,data-entity-uuid,data-entity-embed-display,data-entity-embed-settings]',
+        requiredContent: 'drupal-entity[data-entity-type,data-entity-id,data-entity-uuid,data-entity-embed-display,data-entity-embed-settings]',
 
         // Simply recognize the element as our own. The inner markup if fetched
         // and inserted the init() callback, since it requires the actual DOM

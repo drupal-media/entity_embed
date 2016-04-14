e2c3dce9645dda1b73f3de3475e309e082b42d09
Dave Reid <dave@davereid.net>
2016-04-13 20:05:04 -0500

    Issue #2705611 by Dave Reid: Fixed HTMLElement object should be passed to Drupal.ajax, not a jQuery object. (#213)

--- js/plugins/drupalentity/plugin.js
+++ js/plugins/drupalentity/plugin.js
@@ -107,9 +107,9 @@
           var $element = $(element.$);
           // Use the Ajax framework to fetch the HTML, so that we can retrieve
           // out-of-band assets (JS, CSS...).
-          var entityEmbedPreview = new Drupal.ajax({
+          var entityEmbedPreview = Drupal.ajax({
             base: $element.attr('id'),
-            element: $element,
+            element: $element.get(0),
             url: Drupal.url('embed/preview/' + editor.config.drupal.format + '?' + $.param({
               value: element.getOuterHtml()
             })),
